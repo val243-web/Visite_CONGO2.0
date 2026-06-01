1b33cdd002906cf913953f59577c59eefe1ff446
valéry <valeryiraginsh@gmail.com>
2026-06-02 01:45:58 +0200

    Création de la page d'accueil avec son style

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const expresse = require("express");
+const express = require("express");
+const { listeParcsRDC } = require("./public/parcsModule.js");
 
-const app = expresse();
+const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.render("accueil", { pageTitle: "accueil" });
 });
+
+app.get("/apropos", (req, res) => {
+  res.render("apropos", { pageTitle: "apropos" });
+});
+
+app.get("/parcs", (req, res) => {
+  res.render("parcs", { pageTitle: "parcs"});
+});
+
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
---
+++ public/script.js
@@ -0,0 +1,6 @@
+const date = document.querySelector(".date")
+
+
+// Déclaration de la variable et affectation de la date dans la variable date 
+let dateActuelle = new Date
+date.innerHTML = dateActuelle.getFullYear()
